1751f7583f58d7bea4464717dd3f39b7ec22bcae
ebeltz <ebbeltz@gmail.com>
2021-11-14 18:45:41 -0500

    add the rest of the javascript coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         localStorage.setItem(time, text);
     })
 
-    function timeKeeper() {
+    function timeTracker() {
         var timeNow = moment().hour();
 
         $(".time-block").each(function() {
@@ -19,7 +19,30 @@ $(document).ready(function() {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
+            } else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
             }
         })
     }
+
+    $("#hour8am .description").val(localStorage.getItem("hour8am"));
+    $("#hour9am .description").val(localStorage.getItem("hour9am"));
+    $("#hour10am .description").val(localStorage.getItem("hour10am"));
+    $("#hour11am .description").val(localStorage.getItem("hour11am"));
+    $("#hour12pm .description").val(localStorage.getItem("hour12pm"));
+    $("#hour1pm .description").val(localStorage.getItem("hour1pm"));
+    $("#hour2pm .description").val(localStorage.getItem("hour2pm"));
+    $("#hour3pm .description").val(localStorage.getItem("hour3pm"));
+    $("#hour4pm .description").val(localStorage.getItem("hour4pm"));
+    $("#hour5pm .description").val(localStorage.getItem("hour5pm"));
+    $("#hour6pm .description").val(localStorage.getItem("hour6pm"));
+    $("#hour7pm .description").val(localStorage.getItem("hour7pm"));
+
+    timeTracker();
 })
