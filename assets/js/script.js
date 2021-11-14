e4144547f811365ff2103c64e5b850ade82f8a11
ebeltz <ebbeltz@gmail.com>
2021-11-14 14:55:19 -0900

    trying to get it to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,36 @@
+// Get today's day and date to display at the top of the calendar
 var todaysDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todaysDate);
 
 $(document).ready(function() {
+    // click listener for the save button
     $(".saveBtn").on("click", function() {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
+        // Save text to local storage
         localStorage.setItem(time, text);
     })
 
-    function timeTracker() {
+    function timeKeeper() {
+        // get current number of hours
         var timeNow = moment().hour();
 
         $(".time-block").each(function() {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+            // Adding the classes for the background indicators based on the current time
             if (blockTime < timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
-            } else if (blockTime === timeNow) {
+            }
+            else if (blockTime === timeNow) {
                 $(this).removeClass("past");
                 $(this).removeClass("future");
                 $(this).addClass("present");
-            } else {
+            }
+            else {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
@@ -31,6 +38,7 @@ $(document).ready(function() {
         })
     }
 
+    // Pulls items from local storage if user has saved anything
     $("#hour8am .description").val(localStorage.getItem("hour8am"));
     $("#hour9am .description").val(localStorage.getItem("hour9am"));
     $("#hour10am .description").val(localStorage.getItem("hour10am"));
@@ -44,5 +52,5 @@ $(document).ready(function() {
     $("#hour6pm .description").val(localStorage.getItem("hour6pm"));
     $("#hour7pm .description").val(localStorage.getItem("hour7pm"));
 
-    timeTracker();
+    timeKeeper();
 })
