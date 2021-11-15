0f9ac9bdade2d869af9cc7744ad6040d242ab923
ebeltz <ebbeltz@gmail.com>
2021-11-14 19:08:55 -0500

    updated the README file

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,6 @@ $(document).ready(function() {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
-    $("#hour18 .description").val(localStorage.getItem("hour18"));
-    $("#hour19 .description").val(localStorage.getItem("hour19"));
 
     timeKeeper();
 })
